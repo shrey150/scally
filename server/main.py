a3c0a5049a9795e7374f251c250143c50d550a75
Shrey Pandya <shrey150@yahoo.com>
2024-06-01 17:03:51 -0700

    refactor backend & add server

---
+++ server/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+def main():
+    # TODO add envvars in prod
+    uvicorn.run(
+        app="server:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
+
+if __name__ == '__main__':
+    main()
